5d7a896cb41a59cf9dd31f0d5fa7490b30a8ea71
reiulen <reiandika10@gmail.com>
2023-03-26 15:11:08 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login | Cikarang Lake View Premium</title>
+    <title>Login | SISTEM INFORMASI AKADEMIK PRODI</title>
     <link rel="icon" type="image/png" href="/assets/gambar/logo.png" />
 
     <!-- Google Font: Source Sans Pro -->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title> {{ $title }}</title>
+    <title> {{ $title }} | SISTEM INFORMASI AKADEMIK PRODI</title>
     <link rel="icon" type="image/png" href="" />
 
     <!-- Font Google -->
