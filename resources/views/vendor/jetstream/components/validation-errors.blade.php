50b0b1889a847d9ce4bcaf6641260ea21cb6e603
reiulen <reiandika10@gmail.com>
2023-04-03 09:12:58 +0700

    update

--- resources/views/vendor/jetstream/components/validation-errors.blade.php
+++ resources/views/vendor/jetstream/components/validation-errors.blade.php
@@ -1,6 +1,6 @@
 @if ($errors->any())
     <div {!! $attributes->merge(['class' => 'alert alert-danger text-sm p-2']) !!} role="alert">
-        <div class="font-weight-bold">{{ __('Ada beberapa yang salah .') }}</div>
+        <div class="font-weight-bold">{{ __('Ada beberapa yang salah.') }}</div>
 
         <ul>
             @foreach ($errors->all() as $error)
